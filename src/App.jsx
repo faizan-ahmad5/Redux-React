64b827217efb5dc86b672d27651f29598aba7416
Faizan-Ahmad-Khan <fa3n20004@gmail.com>
2024-07-06 12:31:38 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Counter } from "./features/counter/counter.jsx";
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
+
